9814359412e2535ae7e09ae086ca8a91e92a339d
HashBrown141 <hashimc185@gmail.com>
2021-12-31 18:56:50 -0500

    MenuPage

---
+++ ui/MyApp.py
@@ -0,0 +1,23 @@
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+class MyGrid(Widget):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
